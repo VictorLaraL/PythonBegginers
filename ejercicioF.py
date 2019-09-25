ce54a1597f2959b97df75fdb6ebfae4b3c62eeb5
Victor Lara <vicblack15@hotmail.com>
2019-09-24 22:38:24 -0500

    add ejercicioF

---
+++ ejercicioF.py
@@ -0,0 +1,16 @@
+''' 
+Ejercicio para la creacion de una lista de alumnos con sus respectivas caificaciones del semestre: 
+*   Ingresaremos el nombre de pila del alumno y su primer apellido, seguido de el numero de materias que llevo en el      semestre. [el numero de materias debe ser mayor a 0]
+*   Pediremos la calificacion de cada materia y calcularemos su promedio de ese semestre si es mayor a 70                almacenaremos TRUE y si es menor a 70 FALSE.[las calificaciones no pueden ser negativas ni estar por  encima del     100, perosi pueden ser 0] 
+*   Los datos los almacenaremos en una lista (nombre, apellido, calificacion, A/R) y al final crearemos una lista de     alumnos. [la lista de alumnos estar en un dicionario y la clave de cada alumno sera su nombre] 
+*   El main contendra el menu para agregar los alumnos que el usuario desee hasta que se interrumpa la ejecucion         ingresando xxx en el nombre del alumno.
+*   Una vez dejemos de ingresar los datos el programa, se ejecutara la lista de alumnos con sus respectivos datos.       [los datos que deben aparecer deben ser de esta forma: nombre    apellido    calificacion    A/R]
+*   El ejercicio lo ejecutaremos en funciones:'''
+
+
+def agregarAlumno(alumno):
+    pass
+
+def calculoPromedio(calificaciones):
+    pass
+
